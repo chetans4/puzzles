c7f1ab3efbd267db5aac4589b96fa5ddd2042e2e
Chetan S Choudhary <chetan.choudhary@metacube.com>
2018-06-20 09:12:31 +0530

    test double subtract.

--- src/org/chetan/main/PuzzleMain.java
+++ src/org/chetan/main/PuzzleMain.java
@@ -1,10 +1,13 @@
 package org.chetan.main;
 
+import java.math.BigDecimal;
+
 public class PuzzleMain {
 
 	public static void main(String[] args) {
 		
-		
+		System.out.println(2.00 - 1.10);
+		System.out.println(new BigDecimal("2.00").subtract(new BigDecimal("1.10")));
 		
 	}
 
