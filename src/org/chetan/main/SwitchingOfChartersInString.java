a93942b514a035d6334af99a2998872380bfdbb5
Chetan S. Choudhary <chetan.choudhary@metacube.com>
2018-09-22 17:32:08 +0530

    SwitchingOfChartersInString

--- src/org/chetan/main/SwitchingOfChartersInString.java
+++ src/org/chetan/main/SwitchingOfChartersInString.java
@@ -2,29 +2,29 @@ package org.chetan.main;
 
 public class SwitchingOfChartersInString {
 
+	//chetan > ehcnat
 	public static void main(String[] args) {
 		
 //		switchWithNext();
-//		switchWithAlternate();
-	
-		changeCharAndLengthOnRuntimeForLoop();
+//		switchWithAlternateAllIndex();
+		switchWithAlternateOneCharAtATime();
+//		changeCharAndLengthOnRuntimeForLoop();
 	}
 
-	private static void changeCharAndLengthOnRuntimeForLoop() {
-		
-		StringBuilder altStr = new StringBuilder("1234");
-		altStr.append("567");
-		System.out.println("length : "+altStr.length());
-		int i=0; 
-		for(;i<altStr.length(); i++){
-			System.out.println("index : "+i);
-//			altStr.deleteCharAt(0); // It works 
-//			altStr.append("89"); // It throws heap space error because, every time the loop execute the length will increase with 2
+	private static void switchWithAlternateOneCharAtATime() {
+		String str = "chetan";
+		char[] charArr =  str.toCharArray();
+		for(int i=0; i<charArr.length; i++){
+			if((i+1) % 3 == 0){
+				char temp = charArr[i];
+				charArr[i] = charArr[i-2];
+				charArr[i-2] = temp;
+			}
 		}
-		System.out.println("length : "+i);
+		System.out.println("alternate--switch : "+new String(charArr));
 	}
 
-	private static void switchWithAlternate() {
+	private static void switchWithAlternateAllIndex() {
 		String str = "chetan";
 		char[] altStr = str.toCharArray();
 		
@@ -45,21 +45,46 @@ public class SwitchingOfChartersInString {
 	}
 
 	private static void switchWithNext() {
+		
+		String str = "chetan";
+		for (int i = 0; i < str.length(); i++) {
+			if(i%2 == 0 && i+1 < str.length()){
+				System.out.println("str-- "+i+" : "+str);
+				str = str.substring(0,i) + str.charAt(i+1) +str.charAt(i)  + str.substring(i+2);
+			}
+		}
+		
+		System.out.println("str : "+str);
+//		switchWithNextSB();
+	}
+	
+	private static void switchWithNextSB() {
 		String str = "chetans";
 		StringBuilder altStr = new StringBuilder();
-		
 		for(int i=0; i<str.length(); i++){
-			
 			if(i % 2 == 0){
 				if(i+1 < str.length()){
 					altStr.append(str.charAt(i+1));
 				}
-				
 				altStr.append(str.charAt(i));
 			}
-			
 		}
-		
 		System.out.println("altered String : "+altStr);
 	}
+	
+
+	private static void changeCharAndLengthOnRuntimeForLoop() {
+		
+		StringBuilder altStr = new StringBuilder("1234");
+		altStr.append("567");
+		System.out.println("length : "+altStr.length());
+		int i=0; 
+		for(;i<altStr.length(); i++){
+			System.out.println("index : "+i);
+//			altStr.deleteCharAt(0); // It works 
+//			altStr.append("89"); // It throws heap space error because, every time the loop execute the length will increase with 2
+		}
+		System.out.println("length : "+i);
+	}
+
 }
